05d987dbf0725d02ca8c2c6f605e770dc95a23e2
KoMoon-Developer <cakegelon@gmail.com>
2024-11-29 14:37:54 -0300

    Meu Primeiro Commit

---
+++ Main.cpp
@@ -0,0 +1,122 @@
+#include <glad/glad.h>
+
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+using std::cout;
+using uint = unsigned int;
+
+float vertex[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+int main() {
+  cout << "Hello OpenGL" << '\n';
+  if (!glfwInit()) {
+    cout << "Failed to initialize GLFW.\n";
+    return -1;
+  }
+
+  glfwWindowHint(GLFW_SAMPLES, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow *window;
+  window = glfwCreateWindow(800, 600, "C++ OpenGL on Windows", NULL, NULL);
+  if (window == NULL) {
+    cout << "Failed to open GLFW window.\n";
+    return 1;
+  }
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    cout << "Failed to initialize GLAD.\n";
+    return 1;
+  }
+
+  glViewport(0, 0, 800, 600);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  // Generate Vertex buffer and Vertex array
+  uint VAO, VBO;
+  glGenVertexArrays(1, &VBO);
+  glGenBuffers(1, &VAO);
+
+  // Bind
+  glBindVertexArray(VAO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+  // Insert Data
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertex), vertex, GL_STATIC_DRAW);
+
+  // Config Data viewer
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  // Unbind
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);
+
+  // shaders
+  const char *vertexShaderSource = R"(
+    #version 460 core
+    layout (location = 0) in vec3 aPos;
+
+    void main() {
+    gl_Position = vec4(aPos, 1.0);
+    }
+  )";
+
+  const char *fragmentShaderSource = R"( 
+    #version 460 core
+    out vec4 FragColor;
+
+    void main() {
+    FragColor = vec4(1.0, 0.5, 0.2, 1.0);  // Laranja
+    }
+  )";
+
+  // Compile Shaders
+  unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+
+  unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+
+  // Create Program
+  unsigned int shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  // clear shaders
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  while (!glfwWindowShouldClose(window)) {
+
+    // Clear Screen
+    glClearColor(0.0f, 1.0f, 0.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Draw Triangle
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteProgram(shaderProgram);
+
+  glfwTerminate();
+  return 0;
+}
